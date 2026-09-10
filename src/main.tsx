4f9f7c6100347dc1c3e41c4c61c46b223f2f8357
李丹 <dan.li01@raytrontek.com>
2026-09-10 09:45:56 +0800

    fix: support github pages router base

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { UiSettingsProvider } from './contexts/UiSettingsContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
       <UiSettingsProvider><App /></UiSettingsProvider>
     </BrowserRouter>
   </StrictMode>,
